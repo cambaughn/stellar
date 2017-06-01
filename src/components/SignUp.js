308ca66f3ff1547689329d2e53c363fd285e1165
Cameron Baughn <cam.baughn@gmail.com>
2017-06-01 00:11:03 -0700

    Fix sign in - now functions working

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,7 +6,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import stylePresets from '../util/stylePresets';
-import { signUp } from '../util/signInHelpers';
+import signInHelpers from '../util/signInHelpers';
 
 
 class SignUp extends Component {
@@ -22,15 +22,15 @@ class SignUp extends Component {
   }
 
   handleSubmit(event) {
-    console.log('youooouu')
     event.preventDefault();
-    signUp({ name: this.state.name, email: this.state.email, password: this.state.email }, console.log)
+    event.stopPropagation();
+    signInHelpers.signUp({ name: this.state.name, email: this.state.email, password: this.state.email }, console.log)
   }
 
   render() {
     return (
       <div>
-        <form style={stylePresets.form}>
+        <form style={stylePresets.form} onSubmit={this.handleSubmit.bind(this)}>
           <input type="text" placeholder="Full Name" style={stylePresets.textInput}
             onChange={e => { this.setState({ 'name': e.target.value }) }} />
 
@@ -41,8 +41,7 @@ class SignUp extends Component {
             onChange={e => { this.setState({ 'password': e.target.value }) }} />
 
           <input type="password" placeholder="Confirm Password" style={stylePresets.textInput} />
-          <button type="submit" style={stylePresets.buttonPrimary}
-            onSubmit={this.handleSubmit.bind(this)}>
+          <button type="submit" style={stylePresets.buttonPrimary}>
             Sign Up
           </button>
         </form>
